3eadb5dba49bb6eaa22a1e5478007db05b497706
kgomathisankari <79976845+kgomathisankari@users.noreply.github.com>
2022-03-29 12:11:21 +0530

    Update SampleSpringbootAppApplication.java
    Add print statement to test pull

--- src/main/java/com/example/samplespringbootapp/SampleSpringbootAppApplication.java
+++ src/main/java/com/example/samplespringbootapp/SampleSpringbootAppApplication.java
@@ -8,6 +8,7 @@ public class SampleSpringbootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringbootAppApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
