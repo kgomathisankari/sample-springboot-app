dd309faec1daeed1afb7a1fa059c3c4aa3618755
Gomathi Sankari <kgomathisankari@yahoo.co.in>
2022-03-29 11:06:25 +0530

    Base Version

---
+++ src/main/java/com/example/samplespringbootapp/SampleSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootAppApplication.class, args);
+	}
+
+}
